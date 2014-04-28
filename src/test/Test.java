3f159bd5f5c0aafe5c389cdcef9860023f930085
ansj <ansj-sun@163.com>
2014-04-28 18:21:05 +0800

    我也不知道加了什么.好像是sum content

--- src/test/Test.java
+++ src/test/Test.java
@@ -8,11 +8,19 @@ import com.ansj.vec.Word2VEC;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-//        Word2VEC w1 = new Word2VEC() ;
-//        w1.loadJavaModel("library/vector.mod") ;
-//        
-//        System.out.println(w1.distance("奥尼尔"));
+        Word2VEC w1 = new Word2VEC() ;
+        w1.loadGoogleModel("library/corpus.bin") ;
         
-        System.out.println(CRFAnalysis.parse("汤姆克鲁斯的英文名字很苦"));
+        System.out.println(w1.distance("奥尼尔"));
+        
+        System.out.println(w1.distance("毛泽东"));
+        
+        System.out.println(w1.distance("邓小平"));
+        
+        
+        System.out.println(w1.distance("魔术队"));
+        
+        System.out.println(w1.distance("魔术"));
+        
     }
 }
