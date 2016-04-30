59ca053b1ab9c076a4962697bc359e43ed59aa65
elloray <elliott.ray@qq.com>
2016-05-01 01:17:28 +0800

    1. 将Neuron中的freq修改为double类型 2. 在Neuron中增加category属性 3. 支持文本预分类的情况下构造hoffman tree

--- src/com/ansj/vec/domain/WordNeuron.java
+++ src/com/ansj/vec/domain/WordNeuron.java
@@ -6,39 +6,57 @@ import java.util.List;
 import java.util.Random;
 
 public class WordNeuron extends Neuron {
-    public String name;
-    public double[] syn0 = null; //input->hidden
-    public List<Neuron> neurons = null;//路径神经元
-    public int[] codeArr = null;
+  public String name;
+  public double[] syn0 = null; // input->hidden
+  public List<Neuron> neurons = null;// 路径神经元
+  public int[] codeArr = null;
 
-    public List<Neuron> makeNeurons() {
-        if (neurons != null) {
-            return neurons;
-        }
-        Neuron neuron = this;
-        neurons = new LinkedList<>();
-        while ((neuron = neuron.parent) != null) {
-            neurons.add(neuron);
-        }
-        Collections.reverse(neurons);
-        codeArr = new int[neurons.size()];
-
-        for (int i = 1; i < neurons.size(); i++) {
-            codeArr[i - 1] = neurons.get(i).code;
-        }
-        codeArr[codeArr.length - 1] = this.code;
-
-        return neurons;
+  public List<Neuron> makeNeurons() {
+    if (neurons != null) {
+      return neurons;
     }
-
-    public WordNeuron(String name, int freq, int layerSize) {
-        this.name = name;
-        this.freq = freq;
-        this.syn0 = new double[layerSize];
-        Random random = new Random();
-        for (int i = 0; i < syn0.length; i++) {
-            syn0[i] = (random.nextDouble() - 0.5) / layerSize;
-        }
+    Neuron neuron = this;
+    neurons = new LinkedList<>();
+    while ((neuron = neuron.parent) != null) {
+      neurons.add(neuron);
     }
+    Collections.reverse(neurons);
+    codeArr = new int[neurons.size()];
+
+    for (int i = 1; i < neurons.size(); i++) {
+      codeArr[i - 1] = neurons.get(i).code;
+    }
+    codeArr[codeArr.length - 1] = this.code;
+
+    return neurons;
+  }
+
+  public WordNeuron(String name, double freq, int layerSize) {
+    this.name = name;
+    this.freq = freq;
+    this.syn0 = new double[layerSize];
+    Random random = new Random();
+    for (int i = 0; i < syn0.length; i++) {
+      syn0[i] = (random.nextDouble() - 0.5) / layerSize;
+    }
+  }
+
+  /**
+   * 用于有监督的创造hoffman tree
+   * 
+   * @param name
+   * @param freq
+   * @param layerSize
+   */
+  public WordNeuron(String name, double freq, int category, int layerSize) {
+    this.name = name;
+    this.freq = freq;
+    this.syn0 = new double[layerSize];
+    this.category = category;
+    Random random = new Random();
+    for (int i = 0; i < syn0.length; i++) {
+      syn0[i] = (random.nextDouble() - 0.5) / layerSize;
+    }
+  }
 
 }
